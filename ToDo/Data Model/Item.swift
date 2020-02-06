ea4653cefa91a84b640d3cebd5dff15efed61153
hackeru <hackeru@hackerus-Mac.local>
2020-02-06 02:09:37 -0800

    Convert to mvc design  pattern

---
+++ ToDo/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by hackeru on 06/02/2020.
+//  Copyright © 2020 hackeru. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
